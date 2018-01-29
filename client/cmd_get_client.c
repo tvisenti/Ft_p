9c247ce66b2d647accb0709ca82b60c8438fe5c5
tvisenti <tvisenti@student.42.fr>
2018-01-29 18:01:13 +0100

    [cmd get]: works

--- client/cmd_get_client.c
+++ client/cmd_get_client.c
@@ -6,47 +6,103 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 15:42:30 by tvisenti          #+#    #+#             */
-/*   Updated: 2018/01/17 17:04:27 by tvisenti         ###   ########.fr       */
+/*   Updated: 2018/01/29 17:49:25 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_p.h"
 
+static int		open_file_get_client(char *cmd, int fd)
+{
+	char		*filename;
+	int			file;
+
+	filename = cmd;
+	while (*filename == ' ')
+		++filename;
+	if ((file = open(filename, O_WRONLY | O_CREAT,
+		S_IRWXU | S_IRGRP | S_IROTH)) == -1)
+	{
+		print_error_get_put("Can't create the file, already exists");
+		ft_putendl_fd("ERROR_FD", fd);
+	}
+	else
+		ft_putendl_fd("VERIF_FD", fd);
+	return (file);
+}
+
 static int		recv_get_client(int fd, int file, int size_max)
 {
 	char		*buff;
 	int			size;
-	int			ret;
-	
-	ret = 0;
+	int			n;
+
+	n = 0;
 	if (!(buff = malloc(sizeof(char) * size_max)))
 		return (-1);
-	ft_bzero(buff, size_max);
-	while (ret < size_max)
+	while (n < size_max)
 	{
-		size = recv(fd, buff + ret, 4096, 0);
+		size = recv(fd, buff + n, 4096, 0);
 		if (size % 4096 != 0)
 			break ;
-		ret += size;
+		n += size;
 	}
 	write(file, buff, size_max);
-	write(file, "\0", 1);
 	return (1);
 }
 
-int			cmd_get_client(int fd, char *buf)
+static int		size_file(int fd)
+{
+	int			size;
+	char		*line;
+
+	size = 0;
+	line = NULL;
+	if (get_next_line(fd, &line) == 1)
+	{
+		size = ft_atoi(line);
+		if (size < 1)
+		{
+			ft_putendl(line);
+			ft_putendl_fd("ERROR_FILESIZE", fd);
+			free(line);
+			return (-1);
+		}
+		free(line);
+	}
+	else
+		return (-1);
+	ft_putendl_fd("SEND", fd);
+	return (size);
+}
+
+static void		get_next(int fd)
+{
+	char		*line;
+
+	if (ft_get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+}
+
+void			cmd_get_client(int fd, char *buf)
 {
 	int			file;
 	int			size;
 
-	if ((file = open_file(buf)) == -1)
-		return (print_error("get [client], can't open this file"));
-	if ((size = get_size_file(fd)) == -1)
-		return (print_error("get [client], file doesn't exist"));
-	if (recv_get_client(fd, file, size) == -1)
-		return (print_error("get [client], recv failed"));
-	ft_putendl("\033[32mSUCCESS: get\033[0m");
-	ft_putstr("$> ");
+	if (recv_alert("SERVER_OK", fd) < 1)
+		return (print_error_get_put("open() failed"));
+	if ((file = open_file_get_client(buf, fd)) == -1)
+		return ;
+	if (recv_alert("TEST_OK", fd) < 1)
+		return (print_error_get_put("mmap() failed"));
+	if ((size = size_file(fd)) == -1)
+		return ;
+	recv_get_client(fd, file, size);
+	ft_putendl_fd("SUCCESS", fd);
+	get_next(fd);
 	close(file);
-	return (1);
+	ft_putstr("$> ");
 }
--- client/cmd_put_client.c
+++ client/cmd_put_client.c
@@ -6,46 +6,151 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 15:42:30 by tvisenti          #+#    #+#             */
-/*   Updated: 2018/01/17 17:04:27 by tvisenti         ###   ########.fr       */
+/*   Updated: 2018/01/29 17:53:10 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_p.h"
 
-static int		send_put_client(struct stat st, int fd, void *ptr, int file)
+static int		open_file_put_client(char *cmd, int fd)
+{
+	char		*filename;
+	int			file;
+
+	filename = cmd;
+	while (*filename == ' ')
+		++filename;
+	if ((file = open(filename, O_RDONLY)) == -1)
+		ft_putendl_fd("ERROR_FD", fd);
+	else
+		ft_putendl_fd("CLIENT_OK", fd);
+	return (file);
+}
+
+static void		run_put_client(struct stat st, int client, void *ptr, int file)
 {
 	char		*size;
 
 	size = ft_itoa(st.st_size);
-	ft_putendl_fd(size, fd);
-	write(fd, "\0", 1);
+	ft_putendl_fd(size, client);
 	free(size);
-	if (send(fd, ptr, st.st_size, 0) == -1)
-		return (-1);
-	send(fd, "\0", 1, 0);
+	if (recv_alert("SEND", client) < 1)
+		return ;
+	send(client, ptr, st.st_size, 0);
 	munmap(ptr, st.st_size);
 	close(file);
-	return (1);
+	if (recv_alert("SUCCESS", client) == 1)
+		ft_putendl("\033[32mSUCCESS: put\033[0m");
 }
 
-int			cmd_put_client(int fd, char *buf)
+void			cmd_put_client(int fd, char *buf)
 {
 	int			file;
 	struct stat	st;
 	void		*ptr;
 
-	if ((file = open_file_read(buf)) == -1)
-		return (print_error("put [client], open returns -1"));
+	if ((file = open_file_put_client(buf, fd)) == -1)
+		return (print_error_get_put("open() failed"));
+	if (recv_alert("VERIF_FD", fd) < 1)
+		return (print_error_get_put("can't create the file, already exists"));
 	if ((fstat(file, &st)) == -1)
-		return (print_error("put [client], fstat returns -1"));
-	if ((ptr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file, 0)) 
-	== MAP_FAILED)
-		return (print_error("put [client], mmap returns -1"));
-	if (send_put_client(st, fd, ptr, file) == -1)
-		return (print_error("put [client], fail to send"));
-	ft_putendl("\033[32mSUCCESS: put\033[0m");
-	ft_putstr("$> ");
-	close(file);
-	return (1);
+		return (print_fd_err("fstat() failed", fd));
+	if ((ptr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file, 0))
+			== MAP_FAILED)
+		return (print_fd_err("mmap() failed", fd));
+	ft_putendl_fd("TEST_OK", fd);
+	run_put_client(st, fd, ptr, fd);
 }
+
+
+	// static int		open_file_put_client(char *cmd, int client)
+	// {
+	// 	char		*filename;
+	// 	int			file;
+
+	// 	filename = cmd;
+	// 	while (*filename == ' ')
+	// 		++filename;
+	// 	if ((file = open(filename, O_RDONLY)) == -1)
+	// 		ft_putendl_fd("ERROR_FD", client);
+	// 	else
+	// 		ft_putendl_fd("SERVER_OK", client);
+	// 	return (file);
+	// }
+
+	// static void		send_put_client(int fd, struct stat st, void *ptr)
+	// {
+	// 	char		*size;
+
+	// 	size = ft_itoa(st.st_size);
+	// 	ft_putendl_fd(size, fd);
+	// 	write(fd, "/0", 1);
+	// 	free(size);
+	// 	if (recv_alert("SEND", fd) < 1)
+	// 		return ;
+	// 	send(fd, ptr, st.st_size, 0);
+	// 	munmap(ptr, st.st_size);
+	// }
+
+	// void		cmd_put_client(int fd, char *buf)
+	// {
+	// 	int			file;
+	// 	struct stat	st;
+	// 	void		*ptr;
+
+	// 	ft_putendl("SUIS LA");
+	// 	if ((file = open_file_put_client(buf, fd)) == -1)
+	// 		return (print_error_get_put("open failed"));
+	// 	if (recv_alert("VERIF_FD", fd) < 1)
+	// 		return (print_error_get_put("file already exists"));
+	// 	if ((fstat(file, &st)) == -1)
+	// 		return (print_fd_err("TEST_ERROR", fd));
+	// 	if ((ptr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file, 0))
+	// 			== MAP_FAILED)
+	// 		return (print_fd_err("TEST_ERROR", fd));
+	// 	ft_putendl_fd("TEST_OK", fd);
+	// 	send_put_client(fd, st, ptr);
+	// 	if (recv_alert("SUCCESS", fd) == 1)
+	// 		ft_putendl("\033[32mSUCCESS: put\033[0m");
+	// 	close(file);
+	// 	ft_putstr("$> ");
+	// }
+
+
+// static int		send_put_client(struct stat st, int fd, void *ptr, int file)
+// {
+// 	char		*size;
+
+// 	size = ft_itoa(st.st_size);
+// 	ft_putendl_fd(size, fd);
+// 	write(fd, "\0", 1);
+// 	free(size);
+// 	if (send(fd, ptr, st.st_size, 0) == -1)
+// 		return (-1);
+// 	send(fd, "\0", 1, 0);
+// 	munmap(ptr, st.st_size);
+// 	close(file);
+// 	return (1);
+// }
+
+// int			cmd_put_client(int fd, char *buf)
+// {
+// 	int			file;
+// 	struct stat	st;
+// 	void		*ptr;
+
+// 	if ((file = open_file_read(buf, fd)) == -1)
+// 		return (print_error("put [client], open returns -1"));
+// 	if ((fstat(file, &st)) == -1)
+// 		return (print_error("put [client], fstat returns -1"));
+// 	if ((ptr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file, 0)) 
+// 	== MAP_FAILED)
+// 		return (print_error("put [client], mmap returns -1"));
+// 	if (send_put_client(st, fd, ptr, file) == -1)
+// 		return (print_error("put [client], fail to send"));
+// 	ft_putendl("\033[32mSUCCESS: put\033[0m");
+// 	ft_putstr("$> ");
+// 	close(file);
+// 	return (1);
+// }
 
